acae6fd3e99fcf924fa35a40a121419f1befd0e9
Danil Savchenko <zzappike@gmail.com>
2021-05-19 13:37:49 +0300

    Add wow js

--- group_1/lesson_18/mntn_practice/dist/js.fcffc47e.js
+++ group_1/lesson_18/mntn_practice/dist/js.fcffc47e.js
@@ -118,7 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"../js/index.js":[function(require,module,exports) {
-
+new WOW().init();
 },{}],"C:/Users/Lenovo/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60332" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52828" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
